deced3191030c37ce0a32d8de93040cefd912d23
chayut orapinpatipat <chayut_o@hotmail.com>
2016-03-17 23:08:16 +1100

    Goal 1

--- app/src/main/java/chayut/me/wombatcal/MainActivity.java
+++ app/src/main/java/chayut/me/wombatcal/MainActivity.java
@@ -2,12 +2,16 @@ package chayut.me.wombatcal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    WombatCalLogic calLogic;
+    private static final String TAG = "MainActivity";
 
+    WombatCalLogic calLogic;
+    TextView calDisplay ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,56 +19,57 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         calLogic = new WombatCalLogic();
 
-
+        calDisplay = (TextView) findViewById(R.id.textViewCalDisplay);
     }
 
     public void onButtonPressed(View view){
-        switch (view.getId()) {
+        Log.d(TAG, "onButtonPressed()");
 
+        switch (view.getId()) {
             case R.id.buttonOne:
-                calLogic.numberPress(1);
+                calDisplay.setText(calLogic.numberPress(1));
                 break;
             case R.id.buttonTwo:
-                calLogic.numberPress(2);
+                calDisplay.setText(calLogic.numberPress(2));
                 break;
             case R.id.buttonThree:
-                calLogic.numberPress(3);
+                calDisplay.setText(calLogic.numberPress(3));
                 break;
             case R.id.buttonFour:
-                calLogic.numberPress(4);
+                calDisplay.setText(calLogic.numberPress(4));
                 break;
             case R.id.buttonFive:
-                calLogic.numberPress(5);
+                calDisplay.setText(calLogic.numberPress(5));
                 break;
             case R.id.buttonSix:
-                calLogic.numberPress(6);
+                calDisplay.setText(calLogic.numberPress(6));
                 break;
             case R.id.buttonSeven:
-                calLogic.numberPress(7);
+                calDisplay.setText(calLogic.numberPress(7));
                 break;
             case R.id.buttonEight:
-                calLogic.numberPress(8);
+                calDisplay.setText(calLogic.numberPress(8));
                 break;
             case R.id.buttonNine:
-                calLogic.numberPress(9);
+                calDisplay.setText(calLogic.numberPress(9));
                 break;
             case R.id.buttonZero:
-                calLogic.numberPress(0);
+                calDisplay.setText(calLogic.numberPress(0));
                 break;
             case R.id.buttonPlus:
-                calLogic.opPress(WombatCalLogic.OP_PLUS);
+                calDisplay.setText(calLogic.opPress(WombatCalLogic.OP_PLUS));
                 break;
             case R.id.buttonMinus:
-                calLogic.opPress(WombatCalLogic.OP_MINUS);
+                calDisplay.setText(calLogic.opPress(WombatCalLogic.OP_MINUS));
                 break;
             case R.id.buttonDivide:
-                calLogic.opPress(WombatCalLogic.OP_DIVIDE);
+                calDisplay.setText(calLogic.opPress(WombatCalLogic.OP_DIVIDE));
                 break;
             case R.id.buttonMultiply:
-                calLogic.opPress(WombatCalLogic.OP_MULTIPLY);
+                calDisplay.setText(calLogic.opPress(WombatCalLogic.OP_MULTIPLY));
                 break;
             case R.id.buttonClear:
-                calLogic.clearPress();
+                calDisplay.setText(calLogic.clearPress());
             default:
                 break;
         }
--- app/src/main/java/chayut/me/wombatcal/WombatCalLogic.java
+++ app/src/main/java/chayut/me/wombatcal/WombatCalLogic.java
@@ -7,7 +7,7 @@ import android.util.Log;
  */
 public class WombatCalLogic {
 
-    private static String TAG = "WombatCalLogic";
+    private static final String TAG = "WombatCalLogic";
 
     private static final int WAIT_FOR_FIRST_NUMBER = 1;
     private static final int WAIT_FOR_OPERATOR = 2;
@@ -17,6 +17,8 @@ public class WombatCalLogic {
     public static final int OP_MINUS =2;
     public static final int OP_MULTIPLY=3;
     public static final int OP_DIVIDE =4;
+
+    private static final String[] opSym = {"+","-","*","/"};
 
     private int firstNumber;
     private int secondNumber;
@@ -28,12 +30,13 @@ public class WombatCalLogic {
         this.clearPress();
     }
 
-    public void clearPress(){
+    public String clearPress(){
         Log.d(TAG,"clearPress()");
         firstNumber = 0;
         secondNumber =0;
         opCode = 0;
         calState = WAIT_FOR_FIRST_NUMBER;
+        return "";
     }
     public String numberPress (int number){
         Log.d(TAG,"numberPress()" + number );
@@ -53,6 +56,30 @@ public class WombatCalLogic {
             case WAIT_For_SECOND_NUMBER:
                 secondNumber = number;
                 //calculate stuff
+                float result;
+                switch (opCode){
+                    case OP_PLUS:
+                        result = firstNumber+secondNumber;
+                        outString = String.format("%d %s %d = %.0f",firstNumber,opSym[opCode-1],secondNumber,result);
+                        break;
+                    case  OP_MINUS:
+                        result = firstNumber-secondNumber;
+                        outString = String.format("%d %s %d = %.0f",firstNumber,opSym[opCode-1],secondNumber,result);
+                        break;
+                    case OP_MULTIPLY:
+                        result = firstNumber*secondNumber;
+                        outString = String.format("%d %s %d = %.0f",firstNumber,opSym[opCode-1],secondNumber,result);
+                        break;
+                    case OP_DIVIDE:
+                        if(secondNumber != 0) {
+                            result = (float) firstNumber/(float)secondNumber;
+                            outString = String.format("%d %s %d = %.2f",firstNumber,opSym[opCode-1],secondNumber,result);
+                        }
+                        else
+                            outString = "Sorry you can't divide by 0";
+                        break;
+                }
+                calState =WAIT_FOR_FIRST_NUMBER;
                 break;
             default:
                 outString = "Invalid: Logic Error";
@@ -72,6 +99,7 @@ public class WombatCalLogic {
             case WAIT_FOR_OPERATOR:
             case WAIT_For_SECOND_NUMBER:
                 opCode = opCodePressed;
+                outString = String.format("%d %s",firstNumber,opSym[opCode-1]);
                 calState = WAIT_For_SECOND_NUMBER; //change state
                 break;
             default:
